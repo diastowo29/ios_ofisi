baa128ff553c575e96a59c6079cf80fd2021797b
diastowo <diastowo@gmail.com>
2021-11-25 10:56:06 +0700

    Initial Commit

---
+++ ofisi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ofisi
+//
+//  Created by Trees Solutions on 25/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
